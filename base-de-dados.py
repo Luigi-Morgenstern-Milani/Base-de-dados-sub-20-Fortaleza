558ce453e4710be2d8e7b4910cd69b4caa10c59b
Luigi Morgenstern Milani <luigimilani2003@outlook.com>
2025-08-06 17:51:53 -0300

    Update base-de-dados.py

--- base-de-dados.py
+++ base-de-dados.py
@@ -1,170 +1,173 @@
-baseFortaleza = {
-    'Jogador1': {
-        'nome': 'Cássio Penafiel Acosta',
-        'idade': 20,
-        'posicao': 'Goleiro',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 17,
-        'total-de-minutos-jogados': 1530,
-        'gols': 0,
-        'assistencias': 0
+base_fortaleza = [
+    {
+        "nome": "Cássio Penafiel Acosta",
+        "idade": 20,
+        "posicao": "Goleiro",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 17,
+        "total_de_minutos_jogados": 1530,
+        "gols": 0,
+        "assistencias": 0
     },
-    'Jogador2': {
-        'nome': 'Guilherme Moura da Fonseca',
-        'idade': 20,
-        'posicao': 'Defensor',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 15,
-        'total-de-minutos-jogados': 1329,
-        'gols': 0,
-        'assistencias': 0
+    {
+        "nome": "Guilherme Moura da Fonseca",
+        "idade": 20,
+        "posicao": "Defensor",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 15,
+        "total_de_minutos_jogados": 1329,
+        "gols": 0,
+        "assistencias": 0
     },
-    'Jogador3': {
-        'nome': 'Victor Kauã Guimarães Rocha',
-        'idade': 18,
-        'posicao': 'Defensor',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 15,
-        'total-de-minutos-jogados': 1188,
-        'gols': 2,
-        'assistencias': 0
+    {
+        "nome": "Victor Kauã Guimarães Rocha",
+        "idade": 18,
+        "posicao": "Defensor",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 15,
+        "total_de_minutos_jogados": 1188,
+        "gols": 2,
+        "assistencias": 0
     },
-    'Jogador4': {
-        'nome': 'Rafael da Silva Vaz',
-        'idade': 19,
-        'posicao': 'Defensor',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 13,
-        'total-de-minutos-jogados': 1139,
-        'gols': 1,
-        'assistencias': 0
+    {
+        "nome": "Rafael da Silva Vaz",
+        "idade": 19,
+        "posicao": "Defensor",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 13,
+        "total_de_minutos_jogados": 1139,
+        "gols": 1,
+        "assistencias": 0
     },
-    'Jogador5': {
-        'nome': 'Arthur Barbosa Moura de Oliveira',
-        'idade': 19,
-        'posicao': 'Defensor',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 13,
-        'total-de-minutos-jogados': 581,
-        'gols': 0,
-        'assistencias': 0
+    {
+        "nome": "Arthur Barbosa Moura de Oliveira",
+        "idade": 19,
+        "posicao": "Defensor",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 13,
+        "total_de_minutos_jogados": 581,
+        "gols": 0,
+        "assistencias": 0
     },
-    'Jogador6': {
-        'nome': 'Lucas Emanoel de Abreu dos Reis',
-        'idade': 19,
-        'posicao': 'Meio-campista',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 17,
-        'total-de-minutos-jogados': 1356,
-        'gols': 4,
-        'assistencias': 0
+    {
+        "nome": "Lucas Emanoel de Abreu dos Reis",
+        "idade": 19,
+        "posicao": "Meio-campista",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 17,
+        "total_de_minutos_jogados": 1356,
+        "gols": 4,
+        "assistencias": 0
     },
-    'Jogador7': {
-        'nome': 'Dimas Cândido de Oliveira Filho',
-        'idade': 20,
-        'posicao': 'Meio-campista',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 18,
-        'total-de-minutos-jogados': 830,
-        'gols': 2,
-        'assistencias': 0
+    {
+        "nome": "Dimas Cândido de Oliveira Filho",
+        "idade": 20,
+        "posicao": "Meio-campista",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 18,
+        "total_de_minutos_jogados": 830,
+        "gols": 2,
+        "assistencias": 0
     },
-    'Jogador8': {
-        'nome': 'João Vitor Hipólito Costa',
-        'idade': 20,
-        'posicao': 'Meio-campista',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 15,
-        'total-de-minutos-jogados': 1000,
-        'gols': 0,
-        'assistencias': 0
+    {
+        "nome": "João Vitor Hipólito Costa",
+        "idade": 20,
+        "posicao": "Meio-campista",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 15,
+        "total_de_minutos_jogados": 1000,
+        "gols": 0,
+        "assistencias": 0
     },
-    'Jogador9': {
-        'nome': 'Landerson Costa Araujo',
-        'idade': 18,
-        'posicao': 'Atacante',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 18,
-        'total-de-minutos-jogados': 1573,
-        'gols': 2,
-        'assistencias': 0
+    {
+        "nome": "Landerson Costa Araujo",
+        "idade": 18,
+        "posicao": "Atacante",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 18,
+        "total_de_minutos_jogados": 1573,
+        "gols": 2,
+        "assistencias": 0
     },
-    'Jogador10': {
-        'nome': 'Tomás Ignacio Roco Latorre',
-        'idade': 19,
-        'posicao': 'Atacante',
-        'nacionalidade': 'Chileno',
-        'numero-de-partidas': 17,
-        'total-de-minutos-jogados': 1062,
-        'gols': 4,
-        'assistencias': 0
+    {
+        "nome": "Tomás Ignacio Roco Latorre",
+        "idade": 19,
+        "posicao": "Atacante",
+        "nacionalidade": "Chileno",
+        "numero_de_partidas": 17,
+        "total_de_minutos_jogados": 1062,
+        "gols": 4,
+        "assistencias": 0
     },
-    'Jogador11': {
-        'nome': 'Henrique Cerri Leite',
-        'idade': 19,
-        'posicao': 'Atacante',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 14,
-        'total-de-minutos-jogados': 611,
-        'gols': 1,
-        'assistencias': 0
+    {
+        "nome": "Henrique Cerri Leite",
+        "idade": 19,
+        "posicao": "Atacante",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 14,
+        "total_de_minutos_jogados": 611,
+        "gols": 1,
+        "assistencias": 0
     },
-    'Jogador12': {
-        'nome': 'César Augusto Braga Rodrígues',
-        'idade': 19,
-        'posicao': 'Goleiro',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 2,
-        'total-de-minutos-jogados': 180,
-        'gols': 0,
-        'assistencias': 0
+    {
+        "nome": "César Augusto Braga Rodrígues",
+        "idade": 19,
+        "posicao": "Goleiro",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 2,
+        "total_de_minutos_jogados": 180,
+        "gols": 0,
+        "assistencias": 0
     },
-    'Jogador13': {
-        'nome': 'Kauan Richard Leal Silva',
-        'idade': 20,
-        'posicao': 'Defensor',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas':11,
-        'total-de-minutos-jogados': 846,
-        'gols': 0,
-        'assistencias': 0
+    {
+        "nome": "Kauan Richard Leal Silva",
+        "idade": 20,
+        "posicao": "Defensor",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 11,
+        "total_de_minutos_jogados": 846,
+        "gols": 0,
+        "assistencias": 0
     },
-    'Jogador14': {
-        'nome': 'João Vitor da Rosa',
-        'idade': 20,
-        'posicao': 'Defensor',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 7,
-        'total-de-minutos-jogados': 437,
-        'gols': 0,
-        'assistencias': 0
+    {
+        "nome": "João Vitor da Rosa",
+        "idade": 20,
+        "posicao": "Defensor",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 7,
+        "total_de_minutos_jogados": 437,
+        "gols": 0,
+        "assistencias": 0
     },
-    'Jogador15': {
-        'nome': 'Jairo Joel Reyes Castillo',
-        'idade':19,
-        'posicao': 'Meio-campista',
-        'nacionalidade': 'Equador',
-        'numero-de-partidas': 12,
-        'total-de-minutos-jogados': 532,
-        'gols': 0,
-        'assistencias': 1
+    {
+        "nome": "Jairo Joel Reyes Castillo",
+        "idade": 19,
+        "posicao": "Meio-campista",
+        "nacionalidade": "Equatoriano",
+        "numero_de_partidas": 12,
+        "total_de_minutos_jogados": 532,
+        "gols": 0,
+        "assistencias": 1
     },
-    'Jogador16': {
-        'nome': 'Bruno Domingos Branco',
-        'idade': 19,
-        'posicao': 'Atacante',
-        'nacionalidade': 'Brasileiro',
-        'numero-de-partidas': 16,
-        'total-de-minutos-jogados': 524,
-        'gols': 1,
-        'assistencias': 0
+    {
+        "nome": "Bruno Domingos Branco",
+        "idade": 19,
+        "posicao": "Atacante",
+        "nacionalidade": "Brasileiro",
+        "numero_de_partidas": 16,
+        "total_de_minutos_jogados": 524,
+        "gols": 1,
+        "assistencias": 0
     }
-}
+]
 
-print(f"Atacante: {baseFortaleza['Jogador9']['nome']}, Gols: {baseFortaleza['Jogador9']['gols']}, Partidas: {baseFortaleza['Jogador9']['numero-de-partidas']}")
+# Exibindo informações específicas conforme o desafio
 
+# Atacante
+print(f"Atacante: {base_fortaleza[8]['nome']}, Gols: {base_fortaleza[8]['gols']}, Partidas: {base_fortaleza[8]['numero_de_partidas']}")
 
-print(f"Zagueiro: {baseFortaleza['Jogador2']['nome']}, Idade: {baseFortaleza['Jogador2']['idade']}, Partidas: {baseFortaleza['Jogador2']['numero-de-partidas']}, Minutos jogados: {baseFortaleza['Jogador2']['total-de-minutos-jogados']}")
+# Defensor (Zagueiro)
+print(f"Zagueiro: {base_fortaleza[1]['nome']}, Idade: {base_fortaleza[1]['idade']}, Partidas: {base_fortaleza[1]['numero_de_partidas']}, Minutos jogados: {base_fortaleza[1]['total_de_minutos_jogados']}")
 
-
-print(f"Meio-campista: {baseFortaleza['Jogador6']['nome']}, Posição: {baseFortaleza['Jogador6']['posicao']}, Nacionalidade: {baseFortaleza['Jogador6']['nacionalidade']}, Assistências: {baseFortaleza['Jogador6']['assistencias']}")
+# Meio-campista
+print(f"Meio-campista: {base_fortaleza[5]['nome']}, Posição: {base_fortaleza[5]['posicao']}, Nacionalidade: {base_fortaleza[5]['nacionalidade']}, Assistências: {base_fortaleza[5]['assistencias']}")
